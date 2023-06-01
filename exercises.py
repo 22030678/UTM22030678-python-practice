064ab7139782305fb90f522aa21d74acae85c0ba
22030678 <utm22030678@utma.edu.mx>
2023-06-01 11:55:09 -0500

    message

--- exercises.py
+++ exercises.py
@@ -57,3 +57,49 @@ allergies['Jose Lopez'] = "pumpkins"
 allergies['Luis de Lira'] = "potatoes"
 # The final list is displayed
 print(allergies)
+
+
+
+
+# OPERATION
+# The user must choose what type of operation he wants to do
+operation = str(input("Enter the basic operation you wish to perform (enter only the symbol: +,-,*,/) :"))
+# If the user chooses this option, a sum will be made
+if operation == '+':
+    # The user must enter two numbers
+    firstNumber = int(input("Enter first number: "))
+    secondNumber = int(input("Enter second number: "))
+    # The sum will be made
+    result = firstNumber+secondNumber
+    # The result is displayed
+    print(result)
+# If the user chooses this option, a subtraction will be made
+elif operation == '-':
+    # The user must enter two numbers
+    firstNumber = int(input("Enter first number: "))
+    secondNumber = int(input("Enter second number: "))
+    # The subtraction will be made
+    result = firstNumber-secondNumber
+    # The result is displayed
+    print(result)
+# If the user chooses this option, a multiplication will be made
+elif operation == '*':
+    # The user must enter two numbers
+    firstNumber = int(input("Enter first number: "))
+    secondNumber = int(input("Enter second number: "))
+    # The multiplication will be made
+    result = firstNumber*secondNumber
+    # The result is displayed
+    print(result)
+# If the user chooses this option, a division will be made
+elif operation == '/':
+    # The user must enter two numbers
+    firstNumber = int(input("Enter first number: "))
+    secondNumber = int(input("Enter second number: "))
+    # The division will be made
+    result = firstNumber/secondNumber
+    # The result is displayed
+    print(result)
+else:
+    # The message is displayed
+    print("You did not request any basic operation")
