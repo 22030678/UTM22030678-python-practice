e66ac4e6c106b98d8006e090f36100d44af53ae3
22030678 <utm22030678@utma.edu.mx>
2023-06-01 11:15:35 -0500

    message

--- exercises.py
+++ exercises.py
@@ -1,1 +1,23 @@
-print("Hola Mundo")
+# LIST
+print("Kindergarten")
+# This is the message displayed
+print("An attendance list of babies in room A123 is shown:")
+print("List")
+# These are the names
+babies = ["Jose Lopez", "Pablo Rodriguez", "Juan de Lira",
+          "Raul Esparza", "Zuly Ponce", "Orlando Torres ", "Sharon De la Rosa"]
+# The list is displayed
+print(babies)
+# A question is asked to confirm the registration of all babies
+question = (input("Do they all have assistance?"))
+# There are two options:
+if question == 'not':
+    # If babies are not on the list, they are added
+    babies.append(input("Enter the name of the baby to take list: "))
+    # The new list is displayed
+    print("Updated list: ")
+    for i in babies:
+        print(i)
+# If all the babies are in the list a last message is shown
+else:
+    print("Everyone has assistance:")
